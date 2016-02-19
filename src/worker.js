ab8abf0fa2892d4369c0bdf75296ac5b3ec1d539
Alex Ray <alex.ray12@gmail.com>
2016-02-19 13:48:19 -0800

    Added syntax highlighting using highlight.js

--- src/worker.js
+++ src/worker.js
@@ -1,8 +1,14 @@
 const marked = require('marked');
+importScripts('//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.1.0/highlight.min.js');
 
-marked.setOptions();
+marked.setOptions({
+    highlight: function(code) {
+        return hljs.highlightAuto(code).value;
+    }
+});
 
 onmessage = function(event) {
+    
     marked(event.data, (err, content) => {
         if (err) throw err;
         postMessage(content);
